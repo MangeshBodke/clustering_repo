5acfa3643f95ae249d6edf34098568ad1c110a70
MangeshBodke <96358910+MangeshBodke@users.noreply.github.com>
2022-08-24 20:04:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-import pickle
+import importlib
 from tkinter import OFF
+import pickle
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import figure
+import mpld3
 from sklearn.model_selection import ParameterGrid
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
-from flask import Flask, request, app, jsonify, url_for, render_template
-
-app = Flask(__name__)
+from flask import Flask, render_template
 
 
 drop_Columns = ['Medical_Keyword_8', 'Medical_Keyword_48', 'Medical_Keyword_23', 'Medical_Keyword_30', 'Medical_Keyword_37', 'Medical_Keyword_36', 'Medical_Keyword_21', 'Medical_Keyword_32', 'Medical_Keyword_24', 'Medical_Keyword_22', 'Medical_Keyword_1', 'Medical_Keyword_4', 'Medical_Keyword_46', 'Medical_Keyword_45', 'Medical_Keyword_33', 'Id', 'Medical_Keyword_15', 'Product_Info_2', 'Medical_Keyword_11', 'Medical_Keyword_43', 'Medical_Keyword_27', 'Medical_Keyword_47', 'Medical_Keyword_19', 'Medical_Keyword_29', 'Medical_Keyword_40', 'Medical_Keyword_38', 'Medical_History_24', 'Medical_Keyword_28', 'Medical_Keyword_14', 'Medical_Keyword_39', 'Medical_Keyword_7', 'Medical_Keyword_10', 'Medical_Keyword_20', 'Medical_Keyword_25', 'Medical_Keyword_12', 'BMI', 'Medical_Keyword_5', 'Medical_Keyword_9', 'Medical_Keyword_16', 'Medical_History_15', 'Medical_Keyword_31', 'Medical_Keyword_35', 'Medical_History_10', 'Medical_Keyword_6', 'Medical_Keyword_13', 'Medical_History_32', 'Medical_Keyword_26', 'Medical_Keyword_2', 'Medical_Keyword_34', 'Medical_Keyword_44', 'Medical_Keyword_42', 'Medical_Keyword_17', 'Medical_Keyword_3', 'Medical_Keyword_41', 'Medical_Keyword_18']
@@ -35,10 +36,10 @@ pca_2_result = pca_model.fit_transform(ins_data)
 data_pca = pd.DataFrame(abs(pca_model.components_), columns=ins_data.columns, index=['PC_1', 'PC_2'])
 
 # print(data_pca)
-print('\n As per PC_1:\n', (data_pca[data_pca > 0.3].iloc[0]).dropna())
-print('\n As per PC_2 \n', (data_pca[data_pca > 0.3]).iloc[1].dropna())
+# print('\n As per PC_1:\n', (data_pca[data_pca > 0.3].iloc[0]).dropna())
+# print('\n As per PC_2 \n', (data_pca[data_pca > 0.3]).iloc[1].dropna())
 
-print(pca_2_result, pca_model)
+# print(pca_2_result, pca_model)
 
 parameters = [2, 3, 4, 5, 10, 15]
 param_grid = ParameterGrid({'n_clusters': parameters})
@@ -65,10 +66,11 @@ optimum_num_clusters = best_grid['n_clusters']
 kmeans_cluster_model.fit(ins_data)
 centroids = kmeans_cluster_model.cluster_centers_
 centroids_pca = pca_model.transform(centroids)
-print(centroids_pca)
+# print(centroids_pca)
 
+app = Flask(__name__)
 @app.route('/')
-@app.route('/index')
+@app.route('/clustering')
 def clustering():
     x = pca_2_result[:, 0]
     y = pca_2_result[:, 1]
@@ -78,10 +80,15 @@ def clustering():
     plt.xlabel('PCA 1')
     plt.ylabel('PCA 2')
     plt.scatter(pca_model.transform(centroids)[:,0], centroids_pca[:,1], marker='x', s=200, linewidths=1.5, color='red', edgecolors='black', lw=1.5)
-    plt.show()
-    fig.savefig('clustering_image\image\clustering.png')
-    return render_template('index.html', user_image = 'clustering.png')
+    mpld3.show()
+    # fig.savefig('clustering_image\image\clustering.png')
+    mpld3.save_html(fig, 'clustering.html')
+    return mpld3.fig_to_html(fig)
 
 
-if __name__=="__main__":
-    app.run(debug=OFF)
+if __name__ == "__main__":
+    import random, threading, webbrowser
+    port = 5000 + random.randint(0, 999)
+    url = "http://127.0.0.1:{0}".format(port)
+    threading.Timer(1.25, lambda: webbrowser.open(url) ).start()
+    app.run(port=port, debug=False)
