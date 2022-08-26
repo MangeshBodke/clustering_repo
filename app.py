1a11856e6c5451464ec84e95e19c9163196b80b5
MangeshBodke <96358910+MangeshBodke@users.noreply.github.com>
2022-08-26 14:07:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ if __name__ == "__main__":
     port = 5000 + random.randint(0, 999)
     url = "http://127.0.0.1:{0}".format(port)
     threading.Timer(1.25, lambda: webbrowser.open(url) ).start()
-    app.run(port=port, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
